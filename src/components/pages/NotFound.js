35312def0f54cc9d0378f87d71e8c712f23201b2
ManuelMarin02 <manuel.marin@finalphasesystems.com>
2020-12-21 21:00:45 -0600

    Push Commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        <p className="lead">The page you are looking for does not exist</p>
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/users/Useritem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Useritem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h1>{login}</h1>
+
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Useritem;
